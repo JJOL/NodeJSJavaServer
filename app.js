a78e18d7663eae3bce3a3d98a762380432b11179
Juan Jo Olivera <JJOL@Juans-MacBook-Pro.local>
2015-12-29 16:46:54 -0600

    Basic Express Done

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 var express = require('express');
-var app = express();
+var path = require('path');
+var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
 var controller = require('./socket.js');
+
+var routes = require('./routes/index');
+
+var app = express();
 controller.init();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get("/", function(req, res) {
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+/*app.get("/", function(req, res) {
     controller.sendHi("Juan", function(status) {
         if(status) {
             res.send("Java Says Hi!");
@@ -14,7 +27,7 @@ app.get("/", function(req, res) {
             res.send("Connection is Dead");
         }
     });
-});
+});*/
 
 app.get('/spawn/:type', function(req, res) {
    controller.sendEvent('spawn', req.params.type, function(status) {
